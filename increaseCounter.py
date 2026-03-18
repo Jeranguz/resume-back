550d676a06c3005172833dc772e182162c3a0bce
Jeranguz <Jeremyu4231@gmail.com>
2026-03-18 09:40:48 -0600

    Add lambda function to increment visitor count in DynamoDB

---
+++ increaseCounter.py
@@ -0,0 +1,29 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+table = dynamodb.Table('cloud-resume-challenge')
+
+def lambda_handler(event, context):
+    response = table.update_item(
+        Key={
+            'id': 'visitor_count'
+        },
+        UpdateExpression='ADD #c :val',
+        ExpressionAttributeNames={
+            '#c': 'count'
+        },
+        ExpressionAttributeValues={
+            ':val': 1
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+    return json.dumps({
+        'statusCode': 200,
+        'body': int(response['Attributes']['count']),
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Methods': 'OPTIONS,GET,PUT,PATCH'
+        }
+    })
